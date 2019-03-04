5a55c4b2456f9db38964fd6bdb31f341b8974ae3
Oleksandr Vyshnevskyi <mrrgoodcat@gmail.com>
2019-03-04 15:05:41 +0200

    Form validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NgForm, FormGroup, FormBuilder, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, FormControl, Validators, ValidatorFn, AbstractControl  } from '@angular/forms';
+
+function passwordChecker(password: string) {
+  return (c: AbstractControl): {[key: string]: boolean} | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value !== password)) {
+      return {'passwordMatch': true};
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-login',
@@ -16,28 +25,30 @@ export class LoginComponent implements OnInit {
   }
 
   loginForm: FormGroup;
-  email = new FormControl('', [Validators.required, Validators.email]);
-  name = new FormControl('', [Validators.minLength(3)]);
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  // name = new FormControl('', [Validators.minLength(3)]);
   firstName: string;
+  password: string;
   hide = true;
 
   getEmailErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
-            'Some error';
+    // return this.email.hasError('required') ? 'You must enter a value' :
+    //     this.email.hasError('email') ? 'Not a valid email' :
+    //         'Some error';
   }
 
   getNameErrorMessage() {
-    return this.name.hasError('required') ? 'You must enter a value' :
-            'First Name is required, and must be at least 3 characters.';
+    // return this.name.hasError('required') ? 'You must enter a value' :
+    //         'First Name is required, and must be at least 3 characters.';
   }
   signIn(loginForm: NgForm) {
 
   }
   ngOnInit() {
     this.loginForm = this.fb.group({
-
-    })
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, passwordChecker('1111')]]
+    });
   }
 
 }
